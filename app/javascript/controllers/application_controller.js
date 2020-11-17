1013c0dc453022123e2542545b7a856dd6e32ebb
VIncent Mostert <46714961+tfvincent@users.noreply.github.com>
2020-11-17 20:02:53 +0100

    Deleted script and fixed layout navbar

--- app/javascript/controllers/application_controller.js
+++ app/javascript/controllers/application_controller.js
@@ -50,29 +50,6 @@ export default class extends Controller {
     // document.body.classList.remove('wait')
   }
 
-  const navSlide = () => {
-    const mobile-nav = document.querySelector('.mobile-nav');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-
-    //Toggle navbar
-    mobile-nav.addEventListener('Click', ()=>{
-      nav.classList.toggle('nav-active');
-
-      //link animation
-      navLinks.forEach((link, index)=>{
-        if(link.style.animation){
-          link.style.animation = ''
-        } else {
-          link.style.animation = `navLinkFade 0.6s ease forwards ${index / 7 + 0.5}s`;
-        }
-      });
-      // Icon animation
-      mobile-nav.classList.toggle('toggle');
-    });
-
-  }
-
-  navSlide();
+  
 
 }
