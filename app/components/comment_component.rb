2f7adbf5a374cb7e68469ad7ab4427cf35d833e9
Leander Lismond <36822732+l5r@users.noreply.github.com>
2020-11-29 02:00:36 +0100

    Improve comments display

--- app/components/comment_component.rb
+++ app/components/comment_component.rb
@@ -1,8 +1,10 @@
 class CommentComponent < ViewComponentReflex::Component
   delegate :can?, :cannot?, to: :helpers
 
-  def initialize(comment:)
+  def initialize(comment:, reactable: true, open: false)
     @comment = comment
+    @reactable = reactable
+    @open = open
   end
 
   def collection_key
