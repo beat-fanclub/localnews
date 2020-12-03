4fd87b856bf52882edd46b6188958df9cff65513
Leander Lismond <36822732+l5r@users.noreply.github.com>
2020-12-04 00:49:00 +0100

    Improve db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,48 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+logger = Rails.logger
+
+def point
+  "POINT(#{rand 2..6.0} #{rand 50..51.0})"
+end
+
+user_count = 20
+logger.info "Creating users..."
+user_count.times do |i|
+  User.create!({
+    name: "User #{i}",
+    email: "user#{i}@example.com",
+    password: "123456"
+  })
+end
 users = User.create!([
   { name: "Alice", email: "alice@example.com", password: "123456" },
   { name: "Bob", email: "bob@example.com", password: "123456" }
 ])
 
-Post.create!([
-  { title: "My post", body: "My best content!", location: "POINT(50 50)",
-    user_id: users.first.id },
-  { title: "My question", body: "My best content!", location: "POINT(0 50)",
-    user_id: users.second.id },
-  { title: "My blog", body: "My best content!", location: "POINT(50 0)",
-    user_id: users.first.id },
-])
+post_count = user_count * 5
+logger.info "Creating posts..."
+post_count.times do |i|
+  created_at = rand(post_count * 5).minutes.ago
+  Post.create!([
+    {
+      title: "My post ##{i}",
+      body: "#{i} is a supreme number. Here's why.",
+      location: point,
+      user_id: User.all.offset(rand(user_count)).first.id,
+      created_at: created_at,
+      updated_at: created_at
+    }
+  ])
+end
+
+vote_count = (post_count * 20)
+logger.info "Creating votes..."
+vote_count.times do |i|
+  User.all.offset(rand(user_count)).first.vote(
+    Post.all.offset(rand(post_count)).first,
+    rand(0..3).zero? ? :down : :up
+  )
+end
 
