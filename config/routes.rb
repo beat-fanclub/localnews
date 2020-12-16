2cf9ca54020a1074f73f2909c7d461d8a3c9dd3d
VIncent Mostert <46714961+tfvincent@users.noreply.github.com>
2020-12-16 14:16:18 +0100

    Added login to facebook features

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   end
   resources :comments, only: [:show, :edit, :update]
   resources :tags
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
