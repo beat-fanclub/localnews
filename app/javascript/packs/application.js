30a64ab6c99c4d2f6618e716581ee63dce0b3eec
Leander Lismond <36822732+l5r@users.noreply.github.com>
2020-12-21 19:11:55 +0100

    General layout tweaks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require rails-social-share-button
+
+import 'rails-social-share-button'
 
 require("@rails/ujs").start()
 require("turbolinks").start()
