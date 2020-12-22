6bb018359a6471604ad0293a4a89b52d0356518c
Leander Lismond <36822732+l5r@users.noreply.github.com>
2020-12-22 03:28:01 +0100

    Fix mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   
-  config.action_mailer.delivery_method = :sendgrd_actionmailer
+  config.action_mailer.delivery_method = :sendgrid_actionmailer
   config.action_mailer.sendgrid_actionmailer_settings = Rails.application.credentials[:sendgrid_actionmailer_settings]
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
