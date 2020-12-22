835136d4d216a71d74f8518fc9222bf7fe7fdfb0
Leander Lismond <36822732+l5r@users.noreply.github.com>
2020-12-22 04:09:52 +0100

    Fix mail credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,9 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   
   config.action_mailer.delivery_method = :sendgrid_actionmailer
-  config.action_mailer.sendgrid_actionmailer_settings = Rails.application.credentials[:sendgrid_actionmailer_settings]
+  config.action_mailer.sendgrid_actionmailer_settings = {
+    api_key: ENV['SENDGRID_API_KEY']
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
